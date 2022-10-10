01ac433888d0f48c4603e42c0b3ee2a490c24217
sadiq1020 <sajidnsubd@gmail.comm>
2022-10-11 03:07:32 +0600

    home page css and responsive done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,15 +8,15 @@ const Home = () => {
     // console.log(quiz);
     return (
         <div>
-            <div className='flex flex-col  md:flex flex-row justify-center items-center sm:mx-20'>
-                <div className='w-48'>
+            <div className='flex flex-col md:flex-row justify-center items-center md:mx-20'>
+                <div className='w-72'>
                     <img src="https://img.freepik.com/premium-vector/quiz-logo-isolate-white-questionnaire-icon-poll-sign-flat-bubble-speech-symbol_185004-212.jpg" alt="" />
                 </div>
                 <div className='mx-20 text-start'>
-                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nam similique qui! Sapiente odio natus, exercitationem maxime sunt aut veniam quod atque necessitatibus pariatur corrupti iusto deserunt voluptas in ratione?</h2>
+                    <h2>Quizzes like these will encourage your's self awareness of progress and self assessment. By taking quizzes, you will get instant feedback on your responses. And this can help you identify areas you need to develop yourselves and highlight progress for you to be proud of.</h2>
                 </div>
             </div>
-            <div>
+            <div className='grid grid-cols-1 gap-2 mt-5 mx-5 lg:grid-cols-4 gap-4'>
                 {
                     quizzes.map(quiz => <Quiz key={quiz.id} quiz={quiz} ></Quiz>)
                 }
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -4,8 +4,13 @@ const Quiz = ({ quiz }) => {
     console.log(quiz);
     const { logo, name, total } = quiz;
     return (
-        <div>
+        <div className='m-10 p-5 bg-blue-200 border-solid border-2  border-sky-500 rounded-lg'>
             <img src={logo} alt="" />
+            <div className='flex justify-between items-center mt-3'>
+                <h3 className='text-3xl'>{name}</h3>
+                <p>Questions: {total}</p>
+            </div>
+            <button className='border-solid border-2 border-sky-500 rounded mt-5 py-2 px-5 bg-sky-300 hover:bg-sky-500 hover:text-white'>Start Quiz</button>
         </div>
     );
 };
